9e35faf098d4a42a819b0f869261a6d4d3ef770b
FOLK <folkadpocket99@gmail.com>
2025-11-20 02:48:35 +0700

    add me

--- Roleover.API/Controllers/AuthController.cs
+++ Roleover.API/Controllers/AuthController.cs
@@ -3,20 +3,39 @@ using Microsoft.AspNetCore.Mvc;
 using Roleover.Application.DTOs;
 using Roleover.Application.Interfaces;
 using Roleover.Application.Exceptions;
+using System.Security.Claims;
 
 namespace Roleover.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class AuthController(IAuthProvider authProvider) : ControllerBase
+public class AuthController(IAuthProvider authProvider, IUserRepository userRepository, IRoleRepository roleRepository) : ControllerBase
 {
     private readonly IAuthProvider _authProvider = authProvider;
+    private readonly IUserRepository _userRepository = userRepository;
+    private readonly IRoleRepository _roleRepository = roleRepository;
 
     [HttpPost("login")]
     public async Task<IActionResult> Login(AuthRequest request)
     {
         var token = await _authProvider.AuthenticateAsync(request.Username, request.Password) ?? throw new UnauthorizedException("Invalid credentials");
         return Ok(new AuthResponse(token));
+    }
+
+    [Authorize]
+    [HttpGet("me")]
+    public async Task<IActionResult> Me()
+    {
+        var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (userId == null)
+            return Unauthorized();
+        var user = await _userRepository.GetByIdAsync(userId);
+        if (user == null)
+            return Unauthorized();
+        var roles = await _roleRepository.GetRolesByUserIdAsync(user.Id);
+        foreach (var role in roles) user.AddRole(role);
+
+        return Ok(user);
     }
 
     [Authorize]
--- Roleover.API/Program.cs
+++ Roleover.API/Program.cs
@@ -52,4 +52,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+await app.RunAsync();
